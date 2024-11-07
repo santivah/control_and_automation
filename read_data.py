ed7cee164b4b20a73dca9b6de3fa6a834ebccae3
hmacwalker <hmacwalker1@gmail.com>
2024-11-07 17:33:50 +0100

    get code to actually work

--- read_data.py
+++ read_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 
 # Set up the serial connection (adjust 'COM3' to your correct port)
-ser = serial.Serial('COM3', 9600, timeout=1)  # Change COM3 to your port
+ser = serial.Serial('COM20', 9600, timeout=1)  # Change COM3 to your port
 time.sleep(2)  # Wait for the Arduino to initialize
 
 # initialize dataframe
@@ -27,7 +27,12 @@ try:
 
             # Append the data to the DataFrame
             new_row = {"Datetime": timestamp, "v1": v1, "v2": v2}
-            df = df.append(new_row, ignore_index=True)
+            # df = df.append(new_row, ignore_index=True)
+
+            new_row=pd.DataFrame([new_row])
+            df=pd.concat([df,new_row],ignore_index=True)
+
+
 except KeyboardInterrupt:
     # Handle manual interruption (e.g., pressing Ctrl+C)
     print("Program interrupted by user.")
--- send_communications.ino/send_communications.ino.ino
+++ send_communications.ino/send_communications.ino.ino
@@ -1,5 +1,5 @@
 int number1 = 42; // First number to print
-pychaint number2 = 17; // Second number to print
+int number2 = 17; // Second number to print
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication at 9600 baud
@@ -11,8 +11,6 @@ void loop() {
   Serial.print(";");
   Serial.print(number2);
   Serial.println(";");
-  number1++;
-  number2++;
 
-  delay(2000); // Wait for 2000 milliseconds (2 seconds) before the next print
+  delay(1000); // Wait for 2000 milliseconds (2 seconds) before the next print
 }
